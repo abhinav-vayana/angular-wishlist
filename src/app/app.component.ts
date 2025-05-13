a25a6403ec1838d575176fbbe40a3c1c529f36d6
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-13 17:35:47 +0530

    separated the filter in another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,29 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, WishListComponent, AddWishFormComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   items: WishItem[] = [];
-  title = 'wishlist';
 
   listFilter: string = '0';
 
   get visibleItems(): WishItem[] {
-    let value = this.listFilter;
-    if (value === '0') {
-      return this.items;
-    } else if (value === '1') {
-      return this.items.filter((item) => item.isComplete === false);
-    } else {
-      return this.items.filter((item) => item.isComplete === true);
-    }
+    const value = this.listFilter;
+    if (value === '0') return this.items;
+    if (value === '1') return this.items.filter((item) => !item.isComplete);
+    return this.items.filter((item) => item.isComplete);
   }
 }
--- src/app/wish-filter/wish-filter.component.ts
+++ src/app/wish-filter/wish-filter.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { WishItem } from '../../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item,
+];
 @Component({
-  selector: 'app-wish-filter',
-  imports: [],
+  selector: 'wish-filter',
+  imports: [FormsModule],
   templateUrl: './wish-filter.component.html',
-  styleUrl: './wish-filter.component.css'
+  styleUrl: './wish-filter.component.css',
 })
 export class WishFilterComponent {
+  listFilter: any = '0';
+  @Output() filter = new EventEmitter<any>();
 
+  changeFilter(value: any) {
+    this.filter.emit(value);
+  }
 }
