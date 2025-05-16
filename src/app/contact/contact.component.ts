4ab90ceb88bd958edfdc73001f6ed9a5d6708467
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-16 14:50:29 +0530

    added validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmail';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -8,9 +9,16 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ContactComponent {
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      createInvalidDomainValidator(['gmail.com', 'yahoo.com']),
+    ]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
   submitForm() {
     console.log(this.contactForm.valid);
