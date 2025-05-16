b06324e12debbeb5a498111e336632d85411a731
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-16 11:41:06 +0530

    added reactive form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   standalone: false,
   styleUrl: './contact.component.css',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm = new FormGroup({
+    senderName: new FormControl(''),
+    senderEmail: new FormControl(''),
+    senderMessage: new FormControl(''),
+  });
+  submitForm() {
+    console.log(this.contactForm.valid);
+  }
+}
